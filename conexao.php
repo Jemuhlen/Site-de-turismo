c2347c9e271e3fef750865ac50eb68e8a0b2e80c
JuanLM7 <104564583+JuanLM7@users.noreply.github.com>
2022-10-16 15:00:04 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'login');
+
+$conexao = mysqli_connect(HOST, USUARIO,SENHA, DB) or die ('Não foi possível se conectar');
+
+
+?>
