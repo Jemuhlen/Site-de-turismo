a67b0d8120f4c0d1c6834811747050e5204b1048
Jéssica Tolksdorf <93948391+jetks@users.noreply.github.com>
2022-12-07 00:08:34 -0300

    Update index.php
    mudei a mensagem de inicio do album

--- index.php
+++ index.php
@@ -93,8 +93,8 @@
 <section class="py-5 text-center container" id="pontos-turisticos">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">Album example</h1>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+        <h1 class="fw-light">Pontos Turísticos</h1>
+        <p class="lead text-muted">Aqui estão alguns lugares que você adoraria conhecer!</p>
       </div>
     </div>
   </section>
